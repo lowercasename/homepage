363206b92222a33c6789fd0bc45fee9402ccaccd
Raphael Kabo <raphaelkabo@gmail.com>
2022-05-11 23:40:37 +0100

    Updated build script for new Orogene version

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-orogene -mdv -i src/pages -o build -t src/template.html -s src/style.css -a src/assets -b src/blog -p src/post_template.html
+orogene -mdvf \
+    -c src/config.toml \
+    -i src/pages \
+    -o build \
+    -t src/template.html \
+    -s src/style.css \
+    -a src/assets \
+    -b src/blog \
+    -p src/post_template.html
